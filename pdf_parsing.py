b81c43f8b010a1d1ba546bd4d5ee13a96bc5001a
m0L3n <m.lenz@mailbox.org>
2022-12-30 20:11:30 +0100

    parsing pdf document in to csv (or table-based form)

---
+++ pdf_parsing.py
@@ -0,0 +1,8 @@
+import os
+from pdf2image import convert_from path
+import glob
+import cv2
+import pztesseract
+
+#source data from:
+# https://files.stlouisfed.org/files/htdocs/publications/page1-econ/2020/12/01/ \unemployment-insurance-a-tried-and-true-safety-net_SE.pdf

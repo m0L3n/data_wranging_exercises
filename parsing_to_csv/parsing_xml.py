3ecd32ee8a02ca44a657d12ee65becbd95ba0343
m0L3n <m.lenz@mailbox.org>
2023-01-02 16:59:01 +0100

    add data source description

--- parsing_to_csv/parsing_xml.py
+++ parsing_to_csv/parsing_xml.py
@@ -1,6 +1,11 @@
 from lxml import etree
 import csv
 
+# This is an example of reading data from an .xml file with Python, using the "lxml" library.
+# first, pip install the lxml library
+# the data used here is an instance of
+# https://api.stlouisfed.org/fred/series/observations?series_id=U6RATE& \ api_key=YOUR_API_KEY_HERE
+
 xml_filename="U6_FRED_data"
 xml_source_file = open("data\\"+xml_filename+".xml","rb")
 

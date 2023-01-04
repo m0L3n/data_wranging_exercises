2d9c18c79e8ec803fef7639b0979d6283e3bdb70
m0L3n <m.lenz@mailbox.org>
2023-01-04 19:35:07 +0100

    add source file and add folder

--- parsing_to_csv/pdf_parsing.py
+++ parsing_to_csv/pdf_parsing.py
@@ -6,3 +6,11 @@ import pztesseract
 
 #source data from:
 # https://files.stlouisfed.org/files/htdocs/publications/page1-econ/2020/12/01/ \unemployment-insurance-a-tried-and-true-safety-net_SE.pdf
+
+name_pdf='SafeyNet'
+source_pdf= 'data/'+name_pdf+'.pdf'
+
+# if folder with the same name does not exist, create on.
+if os.path.isdir(name_pdf) == False:
+    target_folder = os.mkdir(name_pdf)
+
